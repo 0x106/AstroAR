f0e9faaf67d2f62ae3b586ff977e75801c01dd32
Jordan Campbell <jordan.evan.campbell@gmail.com>
2018-11-06 19:53:32 +1300

    add welcome message in webapp

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -5,6 +5,12 @@ import AstroLogo from './images/astro-logo.png'
 
 class App extends Component {
   render() {
+
+    console.log("~~~~ Welcome! ~~~~")
+    console.log("The repo for this website can be found at: https://github.com/Jordan-Campbell/AstroAR")
+    console.log("I built this app while listening to this soundtrack: https://open.spotify.com/album/3vaofIDja1ihEoBF2dl467")
+    console.log("Happy hacking!")
+
     return (
       <div>
         <Editor />
